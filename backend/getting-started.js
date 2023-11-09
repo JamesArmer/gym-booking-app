661aaf63e47cd2a67574bf06cdad55939fa53644
James Armer <jamesarmer2@gmail.com>
2023-11-09 16:56:41 +0530

    feat: add docker compose

--- backend/getting-started.js
+++ backend/getting-started.js
@@ -1,15 +1,16 @@
-// getting-started.js
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 
+const uri = `mongodb+srv://${username}:${password}@cluster0.mtoumbe.mongodb.net/?retryWrites=true&w=majority`;
+
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(
-    `mongodb+srv://${username}:${password}@cluster0.mtoumbe.mongodb.net/?retryWrites=true&w=majority`,
-  );
+  await mongoose.connect(uri);
 
   const kittySchema = new mongoose.Schema({
     name: String,
