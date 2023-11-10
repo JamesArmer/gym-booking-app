38ab03b877cc3bd7b83beced56d38d54043bb936
James Armer <jamesarmer2@gmail.com>
2023-11-10 16:04:12 +0530

    feat: post data on submit

--- frontend/components/buttons/SubmitButton.tsx
+++ frontend/components/buttons/SubmitButton.tsx
@@ -5,7 +5,26 @@ class SubmitButton extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            fetch('http://localhost:3000/users/create', {
+              method: 'POST',
+              headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                user: {
+                  firstName: 'James',
+                  lastName: 'Armer',
+                  dateOfBirth: '2000-01-01',
+                  email: 'blah@blah.com',
+                  phoneNumber: '+91 78458 40347',
+                },
+              }),
+            });
+          }}>
           <Text style={{color: 'white'}}>Submit</Text>
         </TouchableOpacity>
       </View>
